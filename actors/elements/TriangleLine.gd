16acdccde14f08a2b28f29cf04276ae7cdd6335f
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2024-07-01 17:04:51 +0800

    displaying distance between axis

--- actors/elements/TriangleLine.gd
+++ actors/elements/TriangleLine.gd
@@ -18,3 +18,9 @@ func _physics_process(delta):
 	
 	$Line2.points[0] = Vector2(target.global_position.x, center.global_position.y)
 	$Line2.points[1] = target.global_position
+	
+	$Label.global_position = target.global_position
+	
+	var length_x = snapped(Vector2(center.global_position.x, target.global_position.y).distance_to(target.global_position), 0.01)
+	var length_y = snapped(Vector2(target.global_position.x, center.global_position.y).distance_to(target.global_position), 0.01)
+	$Label.text = 'X: %s \nY: %s' % [length_x, length_y]
